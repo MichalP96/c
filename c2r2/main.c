91d94ca5948ecba51b5efbd8190817aad35edbc2
LAP <pekus96@gmail.com>
2018-07-05 20:07:01 +0200

    c2e2

---
+++ c2r2/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int nrDomu;
+    nrDomu = 17;
+
+    printf("Michal, Wroclaw %da.\n", nrDomu);
+
+    printf("Michal, Wroclaw 17a.\n");
+
+    getchar();
+    return 0;
+
+}
