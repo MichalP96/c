a00c263e1f3dcb2a27982af976e0795a8d5c2370
LAP <pekus96@gmail.com>
2018-07-05 20:11:55 +0200

    c2e1

---
+++ c1r2/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void)
+{
+    printf("Michal Pekala\n");
+
+    printf("Michal\nPekala\n");
+
+    printf("Michal ");
+    printf("Pekala");
+
+    getchar();
+    return 0;
+}
